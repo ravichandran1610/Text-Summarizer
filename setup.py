ae0ccd0844fd961c586f35877d37b5c68616d1bb
ravichandran1610 <rravii.r@gmail.com>
2025-03-01 17:40:09 +0530

    requirement and libs installation

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+
+__version__= '0.0.0'
+
+REPO_NAME = 'Text-Summarization'
+AUTH_USER_NAME = 'ravichandran1610'
+SRC_REPO = 'textSummarizer'
+AUTHOR_EMAIL = 'rravii.r@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTH_USER_NAME,
+    description='A small python package for NLP - Text Summarization app',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/{AUTH_USER_NAME}/{REPO_NAME}',
+    project_urls = {
+        "bug Tracker": f'https://github.com/{AUTH_USER_NAME}/{REPO_NAME}/issues'
+    },
+    package_dir={'', 'src'},
+    packages=setuptools.find_packages(where='src')
+)
