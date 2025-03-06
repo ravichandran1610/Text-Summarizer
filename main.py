99c513142a438d9258aeaec9c7711c5eeff0fe18
ravichandran1610 <rravii.r@gmail.com>
2025-03-07 04:51:13 +0530

    Model Training Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 
 
 STAGE_NAME = 'Data Ingestion'
@@ -38,3 +39,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = 'Model Training'
+try:
+    logger.info(f'>>>>> Stage: {STAGE_NAME} started <<<<<<')
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f'>>>>> Stage: {STAGE_NAME} completed <<<<<< \n\nX===========================X')
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
